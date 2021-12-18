b335cd7c4b8ef0e4ce74f1e8e4c2f0130c2b8931
Anvitha Bhat <anvitha.bhat@sap.com>
2021-12-18 20:43:33 +0530

    Added Http security

--- src/main/java/com/company/varnaa/SecurityConfig.java
+++ src/main/java/com/company/varnaa/SecurityConfig.java
@@ -50,6 +50,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         .roles("USER","PATIENT");
     }
 	
-
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+		 http
+         .csrf().disable()
+         .authorizeRequests()
+         .antMatchers("/").permitAll()
+         .antMatchers("/main").permitAll()
+         
+         .antMatchers("/signup").permitAll()
+         .antMatchers("/doctors/**").hasRole("DOCTOR")
+         .antMatchers("/patients/**").hasRole("PATIENT")
+         .antMatchers("/receptionist/**").hasRole("RECEPTIONIST")
+         .antMatchers("/anonymous*").anonymous()
+         .antMatchers("/login*").permitAll()
+         .anyRequest().authenticated()
+         .and()
+         .formLogin()
+   //     .loginPage("/login")
+//         .loginProcessingUrl("/perform_login")
+         .defaultSuccessUrl("/showPostLogin", false)
+         .permitAll()
+         //.failureUrl("/login.html?error=true")
+         //.failureHandler(authenticationFailureHandler())
+         .and()
+         .logout()
+         .logoutSuccessUrl("/")
+         .permitAll();
+	    }
 	
 }
